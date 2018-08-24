18e6dc019b595c0fa03001653c56fbeaadf5dde8
twinie96 <twinie96@gmail.com>
2018-08-24 12:33:57 +0200

    WIP migrations

---
+++ db/migrate/20180824102143_add_user_id_to_tips.rb
@@ -0,0 +1,5 @@
+class AddUserIdToTips < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :trips, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_132556) do
+ActiveRecord::Schema.define(version: 2018_08_24_102143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_08_23_132556) do
     t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 2018_08_23_132556) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trips", "users"
 end
