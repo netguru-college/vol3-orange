0d4abb4c7ce08e793894971b10819500bd8442da
Shaqal7 <shaqal7@gmail.com>
2018-08-24 13:50:43 +0200

    Add navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require popper
+//= require jquery3
 //= require bootstrap
 //= require rails-ujs
-//= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
