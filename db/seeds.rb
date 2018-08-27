d2629fa61c26895442d3c77a03b441137ac72caa
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-08-27 11:49:50 +0200

    adjusted seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,56 @@
 require 'faker'
 require 'as-duration'
 
+User.create(
+  email: 'user@user.com',
+  password: 'Orange2018'
+)
+
 5.times do
   User.create(
-    email: Faker::Internet.email,
-    password: 'Orange2018vol-3'
-  )
-  
-end
-
-5.times do
-  User.first.trips.create(
-    name: Faker::FunnyName.name
+    email: Faker::Internet.unique.email,
+    password: 'Orange2018'
   )
 end
 
-(1..5).to_a.each do |i|
-  trip_start_date = Faker::Date.between(5.days.from_now, 1.year.from_now)
-  place = Place.create(
-    name: Faker::Address.city,
-    start_date: trip_start_date + i,
-    end_date: trip_start_date + i + 1,
-    country: Faker::Address.country
-  )
-  User.first.trips.first.places << place
+User.all.each do |user|
+  5.times do
+    start_date = Faker::Time.between(1.year.ago, 1.year.from_now, :all)
+    user.trips.create(
+      name: Faker::FunnyName.name,
+      start_date: start_date,
+      end_date: start_date + 2.weeks
+    )
+  end
+end
+
+Trip.all.each do |trip|
+  (1..5).to_a.each do |i|
+    trip.places.create(
+      name: Faker::WorldCup.city,
+      country: Faker::Address.country,
+      start_date: trip.start_date + i,
+      end_date: trip.end_date + i + 1
+    )
+  end
 end
 
 Place.all.each do |place|
-  place.transports << Transport.create(
-    type_of_transport: Faker::FunnyName.name,
-    start_location: Faker::Address.city,
-    end_location: place.name,
-    cost: Faker::Number.positive,
-    start_time: place.end_date,
-    end_time: place.end_date + 3.hours
-  )
+  2.times do
+    place.hotels.create(
+      name: Faker::Witcher.monster,
+      start_time: place.start_date,
+      end_time: place.end_date
+    )
+    place.attractions.create(
+      name: Faker::Restaurant.name,
+      start_date: place.start_date,
+      end_date: place.end_date
+    )
+    place.transports.create(
+      type_of_transport: Faker::Dessert.variety,
+      start_time: place.start_date,
+      end_time: place.first.end_date
+    )
+  end
 end
