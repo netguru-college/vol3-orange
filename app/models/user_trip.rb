64922122e71c75779296bf0563182b4b990b86f5
Shaqal7 <shaqal7@gmail.com>
2018-08-27 10:38:16 +0200

    Add validation to user_trip model

--- app/models/user_trip.rb
+++ app/models/user_trip.rb
@@ -3,4 +3,14 @@ class UserTrip < ApplicationRecord
   belongs_to :trip
 
   attribute :email, :string
+  before_validation :set_user_id, if: :email?
+
+  def set_user_id
+    existing_user = User.find_by(email: email)
+    self.user = if existing_user.present?
+      existing_user
+    else
+      User.invite!(email: email)
+    end
+  end
 end
