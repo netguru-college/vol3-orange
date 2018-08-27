4cb9923c3a88d39a5f42330b5262c0a371dcccd2
twinie96 <twinie96@gmail.com>
2018-08-27 16:40:04 +0200

    Destroy trips when only user destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_destroy :destroy_dependent
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :user_trips, dependent: :destroy
   has_many :trips, through: :user_trips
+
+  def destroy_dependent
+    trips.each do |trip|
+      trip.destroy if trip.users.count == 1
+    end
+  end
 end
