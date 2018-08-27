3becb90e2d03fcaefbddd745797278154513d527
twinie96 <twinie96@gmail.com>
2018-08-27 10:50:21 +0200

    Validations minor style corrections

--- spec/models/attraction_spec.rb
+++ spec/models/attraction_spec.rb
@@ -4,5 +4,4 @@ RSpec.describe Attraction, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:start_date) }
   it { should validate_presence_of(:end_date) }
-
 end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -4,5 +4,4 @@ RSpec.describe Hotel, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:start_time) }
   it { should validate_presence_of(:end_time) }
-
 end
