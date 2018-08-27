75afd2690fdfcf13fa8af54b0e694b641cd0e4c8
Shaqal7 <shaqal7@gmail.com>
2018-08-27 10:37:40 +0200

    add create action to user_trip controller

--- app/controllers/trips/user_trips_controller.rb
+++ app/controllers/trips/user_trips_controller.rb
@@ -1,5 +1,25 @@
-class Projects::ProjectUsersController < ApplicationController
-  def create
+class Trips::UserTripsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_trip
 
+  def create
+    trip_user = @trip.user_trips.new(trip_user_params)
+    trip_user.trip = @trip
+
+    if trip_user.save
+        redirect_to @trip, notice: 'Saved!'
+    else
+        redirect_to @trip, notice: 'Failed saving!'
+    end
   end
+
+  private
+
+    def set_trip
+        @trip = current_user.trips.find(params[:trip_id])
+    end
+
+    def trip_user_params
+        params.require(:user_trip).permit(:email)
+    end
 end
