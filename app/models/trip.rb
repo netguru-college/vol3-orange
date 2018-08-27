306f478393cd0e451e20dbb3dc69acaf797fb886
twinie96 <twinie96@gmail.com>
2018-08-27 15:13:15 +0200

    Add dependent destroy

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-  has_many :user_trips
+  has_many :user_trips, dependent: :destroy
   has_many :users, through: :user_trips
   has_many :places, dependent: :destroy
 
