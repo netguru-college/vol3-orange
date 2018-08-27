adb800989144cf706ec9bb28f19fb0663dde1a1c
twinie96 <twinie96@gmail.com>
2018-08-27 14:16:30 +0200

    Add dependent to places and trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,9 @@
 class Trip < ApplicationRecord
-  has_many :user_trips, dependent: :destroy
-  has_many :users, through: :user_trips, dependent: :destroy
+  has_many :user_trips
+  has_many :users, through: :user_trips
   has_many :places, dependent: :destroy
 
   validates :name, presence: true, length: { in: 2..30 }
   validates :start_date, :end_date, presence: true
+
 end
