27e6fa49fea92272be1b5c81554446e67737b54f
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-08-28 11:18:35 +0200

    place model and nested form

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,9 @@ class Place < ApplicationRecord
   has_many :hotels
   has_many :transports
   has_many :attractions
-  has_many :hotels
   belongs_to :trip
   validates :name, :start_date, :end_date, presence: true
+  accepts_nested_attributes_for :hotels
+  accepts_nested_attributes_for :transports
+  accepts_nested_attributes_for :attractions
 end
