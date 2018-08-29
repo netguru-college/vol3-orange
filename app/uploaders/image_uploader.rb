6757492351be393e42697c261b14f1e2ff6ef04b
Shaqal7 <shaqal7@gmail.com>
2018-08-29 22:47:12 +0200

    more fixes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-<<<<<<< HEAD
   #storage :fog
   storage :file
-=======
-  storage :fog
-  #storage :file
->>>>>>> parent of 056c2e3... fix carrierwave
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
