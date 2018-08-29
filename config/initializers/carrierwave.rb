db6932ae58f2c1993955360275fca5e20ecb87e0
Shaqal7 <shaqal7@gmail.com>
2018-08-29 22:46:03 +0200

    revert 2 last commits

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,40 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET'],
-      :region => ENV['S3_REGION'] # Tokyo
-    }
-    config.fog_directory = ENV['S3_BUCKET_NAME']
-    config.storage = :fog
-  else
-    config.storage = :file
-    config.enable_processing = Rails.env.development?
-  end
-end
+# CarrierWave.configure do |config|
+#   if Rails.env.staging? || Rails.env.production?
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#       :provider => 'AWS',
+#       :aws_access_key_id => ENV['S3_KEY'],
+#       :aws_secret_access_key => ENV['S3_SECRET'],
+#       :region => ENV['S3_REGION'] # Tokyo
+#     }
+#     config.fog_directory = ENV['S3_BUCKET_NAME']
+#     config.storage = :fog
+#   else
+#     config.storage = :file
+#     config.enable_processing = Rails.env.development?
+#   end
+# end
+
+# CarrierWave.configure do |config|
+#   if Rails.env.staging? || Rails.env.production?
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#       :provider => 'AWS',
+#       :aws_access_key_id => ENV['S3_KEY'],
+#       :aws_secret_access_key => ENV['S3_SECRET'],
+#       :region => ENV['S3_REGION'] # Tokyo
+#     }
+#     config.fog_directory = ENV['S3_BUCKET_NAME']
+#     config.storage = :fog
+#   else
+#     config.storage = :file
+#     config.enable_processing = Rails.env.development?
+#   end
+# end
 
 # CarrierWave.configure do |config|
 #   config.fog_credentials = {
