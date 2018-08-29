65b384328db0b41c973b35b430417a862086bf4e
twinie96 <twinie96@gmail.com>
2018-08-29 16:09:06 +0200

    Add model date validations

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,18 @@
 class Attraction < ApplicationRecord
   belongs_to :place
-  validates :name, :start_date, :end_date, presence: true
+  validates :name, :end_date, presence: true
+  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validate :start_date_within_place_dates, :end_date_within_place_dates
+
+  def start_date_within_place_dates
+    if place.start_date > start_date
+      errors.add(:start_date, "can't be earlier than #{place.start_date}")
+    end
+  end
+
+  def end_date_within_place_dates
+    if place.end_date < end_date
+      errors.add(:end_date, "can't be later than #{place.end_date}")
+    end
+  end
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,20 @@
 class Hotel < ApplicationRecord
   belongs_to :place
 
-  validates :name, :start_time, :end_time, presence: true
+  validates :name, :start_time, presence: true
+  validates :end_time, presence: true, date: { after_or_equal_to: :start_time }
+  validate :start_time_within_place_dates, :end_time_within_place_dates
+
+  def start_time_within_place_dates
+    if place.start_date > start_time
+      errors.add(:start_time, "can't be earlier than #{place.start_date}")
+    end
+  end
+
+  def end_time_within_place_dates
+    if place.end_date < end_time
+      errors.add(:end_time, "can't be later than #{place.end_date}")
+    end
+  end
+
 end
