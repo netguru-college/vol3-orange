516ea69b82309b1ac31713f41257a826e85cdb1e
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-08-30 12:19:15 +0200

    revert to aws-secret-key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV["S3_ACCESS_KEY"],
-    aws_secret_access_key: ENV["S3_SECRET_KEY"]
+    aws_access_key_id: ENV["aws_access_id"],
+    aws_secret_access_key: ENV["aws_access_secret_key"]
   }
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"
