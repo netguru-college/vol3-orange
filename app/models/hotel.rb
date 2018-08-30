1d010b907e2ed832aedf74a584cdec412603d872
twinie96 <twinie96@gmail.com>
2018-08-30 10:25:30 +0200

    Correct by comments

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -6,13 +6,13 @@ class Hotel < ApplicationRecord
   validate :start_time_within_place_dates, :end_time_within_place_dates
 
   def start_time_within_place_dates
-    if place.start_date > start_time
+    if place.start_date >= start_time
       errors.add(:start_time, "can't be earlier than #{place.start_date}")
     end
   end
 
   def end_time_within_place_dates
-    if place.end_date < end_time
+    if place.end_date <= end_time
       errors.add(:end_time, "can't be later than #{place.end_date}")
     end
   end
--- app/models/transport.rb
+++ app/models/transport.rb
@@ -5,13 +5,13 @@ class Transport < ApplicationRecord
   validate :start_time_within_place_dates, :end_time_within_place_dates
 
   def start_time_within_place_dates
-    if place.start_date > start_time
+    if place.start_date >= start_time
       errors.add(:start_time, "can't be earlier than #{place.start_date}")
     end
   end
 
   def end_time_within_place_dates
-    if place.end_date < end_time
+    if place.end_date <= end_time
       errors.add(:end_time, "can't be later than #{place.end_date}")
     end
   end
