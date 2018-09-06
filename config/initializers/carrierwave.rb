27c1a6bfa76854e12cac53bcdc92e663274ff615
twinie96 <twinie96@gmail.com>
2018-09-06 20:16:11 +0200

    Add region to carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["aws_access_id"],
-    aws_secret_access_key: ENV["aws_access_secret_key"]
+    aws_secret_access_key: ENV["aws_access_secret_key"],
+    region: ENV["S3_REGION"]
   }
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"
