43c103fa8dc8f384cba7ac9b200006a674e76931
twinie96 <twinie96@gmail.com>
2018-09-07 15:16:21 +0200

    Refactor SetDate service

--- app/services/set_date_service.rb
+++ app/services/set_date_service.rb
@@ -3,8 +3,6 @@ class SetDateService
     @object = object
   end
 
-  attr_reader :object
-
   def call
     @object.start_date = @object.start_date.midnight
     @object.end_date = @object.end_date.midnight
