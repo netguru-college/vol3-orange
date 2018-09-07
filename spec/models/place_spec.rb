a64dd4ebc93f4cbdb5ae61d1ccab0265f63c2ce6
twinie96 <twinie96@gmail.com>
2018-09-07 12:44:37 +0200

    Add validation spec for place

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,12 +1,39 @@
-# require 'rails_helper'
-#
-# RSpec.describe Place, type: :model do
-#   it { should validate_presence_of(:name) }
-#
-#   let(:trip) { build(:trip) }
-#   let(:place) { build(:place) }
-#
-#   it 'checks for valid start date' do
-#     expect(place.start_date).to be_present
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Place, type: :model do
+  let(:place) { build(:place) }
+
+  it 'checks for presence of name' do
+    place.name = nil
+    expect(place).not_to be_valid
+    expect(place.errors[:name][0]).
+      to include("can't be blank")
+  end
+
+  it 'checks for start date after end date' do
+    place.end_date = place.start_date - 1
+    expect(place).not_to be_valid
+    expect(place.errors[:end_date][0]).
+      to include("must be after or equal to")
+  end
+
+  it 'checks for place start date after trips' do
+    place.trip.start_date = Time.now
+    place.start_date = Time.zone.yesterday
+    expect(place).not_to be_valid
+    expect(place.errors[:start_date][0]).
+      to include("can't be earlier than")
+    place.start_date = Time.zone.tomorrow
+    expect(place).to be_valid
+  end
+
+  it 'checks for place end date before than trips' do
+    place.trip.end_date = Time.zone.tomorrow
+    place.end_date = Time.zone.tomorrow + 1
+    expect(place).not_to be_valid
+    expect(place.errors[:end_date][0]).
+      to include("can't be later than")
+    place.end_date = Time.now
+    expect(place).to be_valid
+  end
+end
