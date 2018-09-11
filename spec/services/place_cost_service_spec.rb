14ad708b20858c904e30124e7e33e59243eeae01
twinie96 <twinie96@gmail.com>
2018-09-11 15:17:38 +0200

    Add spec for set-date-service

---
+++ spec/services/place_cost_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+# RSpec.describe TripCostService, type: :service do
+#   let(:place) { build(:place) }
+#   subject { described_class.new(place) }
+#
+#   describe '#call' do
+#
+#     before do
+#       allow(subject).to receive_message_chain(
+#         :attributes, :map, :sum).with(no_args)
+#         .with({ |attr| sum_attr_cost(attr) })
+#         .with(no_args).and_return(result)
+#     end
+#
+#     it { expect(subject.call).to be_truthy }
+#
+#   end
+# end
